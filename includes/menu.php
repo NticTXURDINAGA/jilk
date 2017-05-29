1436d22a098a3e45b2948e93fb7b43b8e1d904c2
NticTXURDINAGA <ntic@fptxurdinaga.com>
2017-05-29 11:09:18 +0200

    29-05-2017

--- includes/menu.php
+++ includes/menu.php
@@ -1,52 +1,34 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-                <a class="navbar-brand" href="../admin/index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> JILK Txurdinaga</a>
-      </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> CSV <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="../admin/kursocsv.php"><span class="glyphicon glyphicon-play" aria-hidden="true"></span> Cargar Cursos</a></li>
-              <li><a href="../admin/ikaslecsv.php"><span class="glyphicon glyphicon-play" aria-hidden="true"></span> Cargar Ikasleak</a></li>
-
-
-            </ul>
-          </li>
-
-<!--
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Alumnos <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="../admin/ikaslecsv.php"><span class="glyphicon glyphicon-play" aria-hidden="true"></span> Cargar CSV</a></li>
-              <li><a href="../admin/ikaslefind.php"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Buscar</a></li>
-
-
-            </ul>
-          </li>
-
--->
-
-          <li class="active"><a href="../logout.php">Cerrar Sesion</a></li>
-
-<!--
-          <li><a href="#about">mas</a></li>
-          <li><a href="#contact">mas</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">mas <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li class="dropdown-header">Nav header</li>
-              <li><a href="#">Separated link</a></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li>
-    -->
-        </ul>
-      </div><!--/.nav-collapse -->
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href=""><span class="glyphicon glyphicon-home" aria-hidden="true"></span> JILK Txurdinaga</a>
     </div>
-  </nav>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> CSV <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="../admin/kursocsv.php"><span class="glyphicon glyphicon-play" aria-hidden="true"></span> Cargar Cursos</a></li>
+            <li><a href="../admin/ikaslecsv.php"><span class="glyphicon glyphicon-play" aria-hidden="true"></span> Cargar Ikasleak</a></li>
+
+
+          </ul>
+        </li>
+
+
+        <li class="active"><a href="../logout.php"><span class="glyphicon glyphicon-new-window" ></span> Cerrar Sesion</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
